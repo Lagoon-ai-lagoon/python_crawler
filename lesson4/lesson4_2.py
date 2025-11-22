56c8c4fea63dcc5b88ad06970fdc4923eb31c6cb
roberthsu2003 <roberthsu2003@gmail.com>
2025-11-22 11:57:28 +0800

    html

--- lesson4/lesson4_2.py
+++ lesson4/lesson4_2.py
@@ -1,13 +1,28 @@
 from playwright.sync_api import sync_playwright
 
-
+from time import sleep
 
 def main():
     with sync_playwright() as p:
+        
+        browser = p.chromium.launch(headless=False)
+
+        print(type(browser))
+
+        page = browser.new_page()
+
+        page.goto("https://www.google.com")
+        print(page.title())
+
+        sleep(20)
+
+        browser.close()
+
         print("建立資源")
     print("釋放資源")
 
 
-
+#12
 if __name__ == "__main__":
     main()
+
