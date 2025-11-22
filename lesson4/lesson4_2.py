ec9c2d1db9dc9ab308a2355c2219c43f575c6424
roberthsu2003 <roberthsu2003@gmail.com>
2025-11-22 10:46:40 +0800

    建立資源釋放資源

--- lesson4/lesson4_2.py
+++ lesson4/lesson4_2.py
@@ -1,3 +1,13 @@
 from playwright.sync_api import sync_playwright
 
-print(type(sync_playwright()))
+
+
+def main():
+    with sync_playwright() as p:
+        print("建立資源")
+    print("釋放資源")
+
+
+
+if __name__ == "__main__":
+    main()
